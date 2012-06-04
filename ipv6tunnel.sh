a501d134b2d6ce4677d5e8b03beee68f543d11b5
Roumen Damianoff <roumen@dawebs.com>
2012-06-03 16:05:15 -0800

    added license

--- ipv6tunnel.sh
+++ ipv6tunnel.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# ipv6tunnel.sh
+# Copyright (c) 2011-2012 Roumen Damianoff - http://roumen.me/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 ### BEGIN INIT INFO
 # Provides: ipv6tunnel
 # Required-Start:
@@ -12,9 +28,9 @@
 # Description: ipv6tunnel
 ### END INIT INFO
 
-########
-# VARS #
-########
+###############
+# CONFIG VARS #
+###############
 
 # remote and local tunnel ips
 REMOTE_IP = '123.45.67.89'
@@ -25,9 +41,9 @@ IPV6[0]   = '2001:123:4f56:78a::2/64'
 IPV6[1]   = '2001:123:45fa::1/48'
 IPV6[2]   = '2001:123:45fa::2/48'
 
-##########
-# SCRIPT #
-##########
+###############
+# CORE SCRIPT #
+###############
 
 case "$1" in
 'start')
